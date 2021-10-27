1d66eec2ed8c569a89c519517d0f60df198f145e
Igor.Koloski <koloski_igor@hotmail.com>
2021-10-27 14:53:53 +0200

    Delete

--- data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom');
-
-INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
